bb291cb0198b7cccc4fd4f323e867bc0a9e13dc1
saumy <saumyadivyanjalee529@gmail.com>
2025-09-13 10:57:03 +0530

    ReceptionistDashboard finished

--- src/main/java/lk/ijse/orm/elite_driving_school_management_system/controller/ReceptionistDashboard.java
+++ src/main/java/lk/ijse/orm/elite_driving_school_management_system/controller/ReceptionistDashboard.java
@@ -2,11 +2,22 @@ package lk.ijse.orm.elite_driving_school_management_system.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 
-public class ReceptionistDashboard {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class ReceptionistDashboard  implements Initializable {
+
+
+    public AnchorPane ancMainContainer;
     @FXML
     private AnchorPane ancpanal;
 
@@ -22,23 +33,52 @@ public class ReceptionistDashboard {
     @FXML
     private Button studentId;
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle  resources) {
+        navigateTo("view/Student.fxml");
+
+    }
+
+    public void navigateTo(String x){
+        try{
+            ancMainContainer.getChildren().clear();
+            Parent pane = FXMLLoader.load(getClass().getResource(x));
+
+            if(pane instanceof Region){
+                Region region = (Region)pane;
+                region.prefWidthProperty().bind(ancMainContainer.widthProperty());
+                region.prefHeightProperty().bind(ancMainContainer.heightProperty());
+            }
+            ancMainContainer.getChildren().add(pane);
+
+        }catch(Exception e){
+            new Alert(Alert.AlertType.ERROR,"Oops! Page Not Found").show();
+            e.printStackTrace();
+        }
+
+    }
     @FXML
     void lessonOnAction(ActionEvent event) {
+        navigateTo("view/Lesson.fxml");
 
     }
 
     @FXML
     void logoutOnAction(ActionEvent event) {
+        navigateTo("view/Logout.fxml");
 
     }
 
     @FXML
     void paymentOnAction(ActionEvent event) {
+        navigateTo("view/Payment.fxml");
 
     }
 
     @FXML
     void studentOnAction(ActionEvent event) {
+        navigateTo("view/Student.fxml");
 
     }
 
